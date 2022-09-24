6b850eeb28b70cc79d5fa523fe10e82c41742df5
Karen <inokent1994@gmail.com>
2022-09-24 15:08:00 +0300

    finish the layout

--- travel/slider.js
+++ travel/slider.js
@@ -1,3 +1,20 @@
-new Swiper('.image-slider',{
-    
+new Swiper('.swiper-container',{
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dinamicBullets: true,
+    },
+    spaceBetween: 30,
+    slidesPerView: 2,
+    slidesPerGroup: 1,
+    initialSlide: 1,
+    watchOverflow: true,
+    centeredSlides: true,
+    slideToClickedSlide: true,
+    keyboard: {
+        enable: true,
+        onlyInViewport: true,
+        pageUpDown: true,
+    },
+
 });
